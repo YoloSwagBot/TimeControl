e140334081c9745f89de0b15f71cb627abe904fc
YoloSwagBot <bathcarson@gmail.com>
2023-11-05 06:13:09 -0800

    fixing TimeUtil

--- app/src/main/java/com/appstr/timecontrol/util/TimeUtil.kt
+++ app/src/main/java/com/appstr/timecontrol/util/TimeUtil.kt
@@ -5,17 +5,27 @@ import com.appstr.timecontrol.minute
 import com.appstr.timecontrol.second
 import java.text.SimpleDateFormat
 import java.util.Date
-
+import java.util.concurrent.TimeUnit
 
 
 fun Int.formatTimeToText(): String {
-    
-    val sdf: SimpleDateFormat = when {
-        this > hour-1 -> SimpleDateFormat("h:mm:ss")
-        this > minute-1 -> SimpleDateFormat("m:ss")
-        this >= 9 * second -> SimpleDateFormat("ss")
-        else -> SimpleDateFormat("s")
+
+    val time = this.toLong()
+
+    return when {
+
+        this > hour-1 -> String.format("%2d:%02d:%02d",
+            TimeUnit.MILLISECONDS.toHours(time),
+            TimeUnit.MILLISECONDS.toMinutes(time) % TimeUnit.HOURS.toMinutes(1),
+            TimeUnit.MILLISECONDS.toSeconds(time) % TimeUnit.MINUTES.toSeconds(1))
+
+        this > minute-1 -> String.format("%2d:%02d",
+            TimeUnit.MILLISECONDS.toMinutes(time) % TimeUnit.HOURS.toMinutes(1),
+            TimeUnit.MILLISECONDS.toSeconds(time) % TimeUnit.MINUTES.toSeconds(1))
+
+        else -> String.format("%2d",
+            TimeUnit.MILLISECONDS.toSeconds(time) % TimeUnit.MINUTES.toSeconds(1))
+
     }
 
-    return sdf.format(Date(this.toLong()))
 }
--- app/src/main/java/com/appstr/timecontrol/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/appstr/timecontrol/viewmodel/GameViewModel.kt
@@ -5,6 +5,7 @@ import android.util.TimeUtils
 import androidx.lifecycle.AndroidViewModel
 import com.appstr.timecontrol.hour
 import com.appstr.timecontrol.minute
+import com.appstr.timecontrol.second
 import com.appstr.timecontrol.ui.model.TimeControl
 import com.appstr.timecontrol.ui.theme.black
 import com.appstr.timecontrol.ui.theme.lightGreen
@@ -18,7 +19,7 @@ import java.util.Date
 class GameViewModel(appli: Application) : AndroidViewModel(appli) {
 
     /** Player 1 */
-    private val _player1TimeLeft = MutableStateFlow(hour)
+    private val _player1TimeLeft = MutableStateFlow(59* second)
     val player1TimeLeft = _player1TimeLeft.asSharedFlow()
     private val _player1TextColor = MutableStateFlow(black)
     val player1TextColor = _player1TextColor.asSharedFlow()
@@ -26,7 +27,7 @@ class GameViewModel(appli: Application) : AndroidViewModel(appli) {
     val player1BackgroundColor = _player1BackgroundColor.asSharedFlow()
 
     /** Player 2 */
-    private val _player2TimeLeft = MutableStateFlow(hour)
+    private val _player2TimeLeft = MutableStateFlow(59*minute)
     val player2TimeLeft = _player2TimeLeft.asSharedFlow()
     private val _player2TextColor = MutableStateFlow(black)
     val player2TextColor = _player2TextColor.asSharedFlow()
